25eae893c8c3d5c3664368a97fd2203410eaaa24
Joseph Bae <josephbae@utexas.edu>
2020-08-13 17:22:27 -0500

    Initial project commits

---
+++ jsx/src/index.js
@@ -0,0 +1,26 @@
+// Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//const React = require('react');
+
+
+// Create a react component
+const App = function() { //ES15 () => instead of function()
+const buttonText = {text: 'Click Me!'};
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}> 
+            {buttonText.text}
+             </button>
+        </div>
+    );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
