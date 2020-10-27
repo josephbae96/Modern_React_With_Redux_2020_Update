666b0ac72d8b29a3e422b893331877567eced22b
Joseph Bae <josephbae@utexas.edu>
2020-10-27 16:01:05 -0500

    Section 12 Finished

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const Dropdown = ({options, selected, onSelectedChange}) => {
+const Dropdown = ({ label, options, selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if(ref.current.contains(event.target)){
+                return;
+            }
+
+            setOpen(false);
+        }
+
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
+    }, []);
+
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value){
             return null;
@@ -16,14 +35,19 @@ const Dropdown = ({options, selected, onSelectedChange}) => {
             </div>
         );
     });
+    
+
+
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
-                <label className="label">Select a color</label>
-                <div className="ui selection dropdown visible active">
+                <label className="label">{label}</label>
+                <div onClick={() => setOpen(!open)} 
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
